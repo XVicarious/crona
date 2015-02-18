201b0622918591373d66b1fb5621d838fb222ce1
Brian Maurer <bmaurer@xvss.net>
2015-02-18 14:38:55 -0500

    schedule work

---
+++ admin/timeEdit/change_schedule.php
@@ -0,0 +1,33 @@
+<?php
+require "../admin_functions.php";
+$sqlConnection = createSql();
+if (sessionCheck()) {
+    $id = $_POST['id'];
+    $in = $_POST['in'];
+    $out = $_POST['out'];
+    $department = $_POST['department'];
+    echo $in.'<br>'.$out.'<br>';
+    $query = 'UPDATE employee_schedule SET ';
+    if (isset($in)) {
+        $in = intval($in);
+        $query .= "schedule_in = $in";
+        if (isset($out) || isset($department)) {
+            $query .= ', ';
+        }
+    }
+    if (isset($out)) {
+        $out = intval($out);
+        $query .= "schedule_out = $out";
+        if (isset($department)) {
+            $query .= ', ';
+        }
+    }
+    if (isset($department)) {
+        $department = intval($department);
+        $query .= "schedule_department = $department";
+    }
+    $id = intval($id);
+    $query .= " WHERE schedule_id = $id";
+    echo $query;
+    mysqli_query($sqlConnection, $query);
+}
