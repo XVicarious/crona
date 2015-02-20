81e70ade9fccca70749f01df4244359f8a109051
Brian Maurer <bmaurer@xvss.net>
2015-02-20 13:10:31 -0500

    finally rid of the old SQL stuff

--- backend/sql_props.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sql_server = 'localhost';
-$sql_username = 'bmaurer_pciven';
-$sql_password = '***REMOVED***';
-$sql_database = 'bmaurer_hhemployee';
-
-$sqlConnection = mysqli_connect($sql_server, $sql_username, $sql_password, $sql_database);
