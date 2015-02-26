2d4e466302db5f21621902a8da7e5b7924a74fb3
Brian Maurer <bmaurer@xvss.net>
2015-02-26 17:07:49 -0500

    materialized part 1..................

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@ $_SESSION["lastAction"] = time();
 <head>
     <title></title>
     <link rel="shortcut icon" href="../favicon.ico"/>
-    <link rel="stylesheet" href="../css/style.css" type="text/css">
+    <!--<link rel="stylesheet" href="../css/style.css" type="text/css">-->
     <link rel="stylesheet" href="../css/jquery-ui.css">
     <link rel="stylesheet" href="../css/jquery-ui.theme.css">
     <link rel="stylesheet" href="../css/jquery.contextMenu.css">
@@ -19,6 +19,7 @@ $_SESSION["lastAction"] = time();
     <link rel="stylesheet" href="../css/timecard.css">
     <link rel="stylesheet" href="../css/cellCases.css">
     <link rel="stylesheet" href="../css/materialize.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <script src="../js/lib/jquery.js"></script>
     <script src="../js/lib/materialize.js"></script>
     <script src="../js/lib/underscore.js"></script>
@@ -35,35 +36,62 @@ $_SESSION["lastAction"] = time();
     <script src="../js/admin/functions.async.js"></script>
     <script src="../js/admin/employee_list.js"></script>
     <script src="../js/admin/initialLoad.js"></script>
+    <script>
+        $(function() {
+            $('.button-collapse').sideNav();
+        });
+    </script>
 </head>
-<body class="tc">
-<div id="head"><h2 style="padding:0;margin:0;text-align:center">Crona Timestamp</h2><h3 style="padding:0;margin:0;text-align:center">Administrative Console</h3></div>
-<div id="row">
-    <div class="col s3">
-        <div class="row">
-            <h3 class="sideNavigationCell">Options</h3>
+<body>
+<div>
+    <nav class="grey">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">Crona Timestamp</a>
+            <ul id="slide-out" class="side-nav">
+                <li><a href="#!" id="timecardButton">Timecards</a></li>
+                <li><a href="#!" id="scheduleButton">Schedules</a></li>
+            </ul>
+            <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
         </div>
-        <div class="row">
-            <a id="timecardButton" class="sideNavigationCell">Timecards</a>
+        <!--<div class="col s12 m4 l3 grey lighten-2">
+            <h3 class="row" >Options</h3>
+            <a class="row" id="timecardButton">Timecards</a>
+            <a class="row" id="scheduleButton">Schedules</a>
+            <a class="row" id="addemployeeButton" href="#addEmployee">Add Employees</a>
+            <a class="row" >System Management</a>
+        <span class="row" >
+            Export
+            <form>
+                <div id="exportC"></div><br><input id="exportcsv" type=button value="Export CSV">
+            </form>
+            <div id="exportScript"></div>
+        </span>
+        </div>-->
+    </nav>
+    <div class="row">
+        <div class="col l2 hide-on-med-and-down grey lighten-2">
+            <div class="nav-wrapper">
+                <ul>
+                    <li><a class="center-align" id="timecardButton">Timecards</a></li>
+                    <li><a class="center-align" id="scheduleButton">Schedules</a></li>
+                </ul>
+            </div>
+
+
+
         </div>
-        <div class="row">
-            <a id="scheduleButton" class="sideNavigationCell">Schedules</a>
-        </div>
-        <div class="row">
-            <a id="addemployeeButton" class="sideNavigationCell" href="#addEmployee">Add Employees</a>
-        </div>
-        <div class="row">
-            <a class="sideNavigationCell">System Management</a>
-        </div>
-        <div class="row">
-            <span class="sideNavigationCell">Export<form>
-                    <div id="exportC"></div>
-                    <br><input id="exportcsv" type=button value="Export CSV"></form><div
-                    id="exportScript"></div></span>
+        <div class="col s12 m12 l10 grey lighten-3">
+            <div id="ajaxDiv"></div>
         </div>
     </div>
-    <div id="col s9 ajaxDiv"></div>
 </div>
+<footer class="page-footer grey">
+    <div class="footer-copyright">
+        <div class="container">
+            2015 XVicarious Software Solutions
+        </div>
+    </div>
+</footer>
 <div id="dialog-confirm" class="ui-helper-hidden" title="Delete Timestamp?">
     <p>
         <span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>
