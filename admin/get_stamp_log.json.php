75c581ba98c460dc909bb3d918d237fe81d2705e
Brian Maurer <bmaurer@xvss.net>
2015-04-09 13:07:29 -0400

    admin console work

---
+++ admin/get_stamp_log.json.php
@@ -0,0 +1,14 @@
+<?php
+require 'admin_functions.php';
+$sqlConnection = createSql();
+if (sessionCheck()) {
+    $query = 'SELECT user_id_stamp,timestamp_list.datetime, (SELECT user_name FROM employee_list WHERE employee_list.user_id = timestamp_list.user_id_stamp) AS username
+              FROM timestamp_list
+              ORDER BY timestamp_list.datetime DESC';
+    $queryResult = mysqli_query($sqlConnection, $query);
+    $qarray = [];
+    while (list($userId,$stamp,$username) = mysqli_fetch_row($queryResult)) {
+        array_push($qarray, ['user'=>$username, 'time'=>$stamp]);
+    }
+    echo json_encode($qarray);
+}
