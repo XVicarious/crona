06d60f1600b3dcb613dcb0e835519abfb91cbbc5
Brian Maurer <bmaurer@xvss.net>
2015-05-18 13:47:54 -0400

    removed redundant function

--- function.php
+++ function.php
@@ -6,16 +6,6 @@ function validateLogin($pass, $hashedPass, $salt, $hashMethod = 'sha1')
     }
     return ($hashedPass === sha1($salt . $pass));
 }
-function randomSalt($len = 8)
-{
-    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()_+-=";
-    $stringLength = strlen($chars) - 1;
-    $str = '';
-    for ($i = 0; $i < $len; ++$i) {
-        $str .= $chars[rand(0, $stringLength)];
-    }
-    return $str;
-}
 function createHash($string, $hashMethod = 'sha1', $saltLength = 8)
 {
     $salt = randomSalt($saltLength);
