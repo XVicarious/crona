2944b6d83eae4c809ed940a82dfd4fcc80fe2d1f
Brian Maurer <bmaurer@xvss.net>
2015-05-18 13:51:20 -0400

    likely part of some debugging.  Works anyway.

--- admin/timeEdit/create_stamp.php
+++ admin/timeEdit/create_stamp.php
@@ -3,7 +3,8 @@ date_default_timezone_set('Atlantic/Reykjavik');
 require "../admin_functions.php";
 $sqlConnection = createSql();
 if (sessionCheck()) {
-    $date = date("Y-m-d H:i:s", $_POST['date']);
+    $dtime = $_POST['date'];
+    $date = date("Y-m-d H:i:s", $dtime);
     $userId = $_POST['user'];
     mysqli_query($sqlConnection, "INSERT INTO timestamp_list (user_id_stamp,datetime) VALUES ($userId,'$date')");
     logTransaction($sqlConnection, '?', 'ADD', 0, $date);
