3f639f0245a546a12f2d3c274eb1fa7997ad3b30
Brian Maurer <bmaurer@xvss.net>
2015-05-18 13:42:34 -0400

    questioning my logic

--- admin/admin_console.php
+++ admin/admin_console.php
@@ -2,3 +2,5 @@
 $echoMe = '<a class="cyan lighten-1 waves-effect waves-light btn-large" id="modlog-button">Open Modification Log</a>';
 $echoMe .= '<a class="cyan lighten-1 waves-effect waves-light btn-large" id="timestamp-button">Open Timestamp Log</a>';
 echo $echoMe;
+
+// Does this need to be a PHP file? likely not
