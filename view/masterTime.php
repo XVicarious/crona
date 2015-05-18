c67cadbde7e834d0b98511a1cb36c470cf731700
Brian Maurer <bmaurer@xvss.net>
2015-05-18 13:42:13 -0400

    quick and dirty viewer for an individual timecard

--- view/masterTime.php
+++ view/masterTime.php
@@ -1,138 +1,194 @@
 <?php
+date_default_timezone_set('America/New_York'); // todo: make timezone configurable
 require '../admin/admin_functions.php';
 $sqlConnection = createSql();
-$dateFormat = 'Y-m-d H:i:s';
-$employeeId = $_COOKIE['xvtss'];
-$range = isset($_POST['range']) ? $_POST['range'] : 'this';
-$rangeQuery = '';
-if ($range === 'this') {
-    $todayIs = date($dateFormat, strtotime('next saturday 23:59:59'));
-    $sundayWas = date($dateFormat, strtotime('last sunday'));
-    $rangeQuery = "AND timestamp_list.datetime BETWEEN '$sundayWas' AND '$todayIs'";
-} elseif ($range === 'last') {
-    $twoSundays = date($dateFormat, strtotime('last sunday -1 weeks'));
-    $lastSaturday = date($dateFormat, strtotime('last saturday 23:59:59'));
-    $rangeQuery = "AND timestamp_list.datetime BETWEEN '$twoSundays' AND '$lastSaturday'";
-} elseif ($range === 'next') {
-    $nextSunday = date($dateFormat, strtotime('next sunday'));
-    $twoSaturdayFromThisSaturday = date($dateFormat, strtotime("next saturday 23:59:59 +1 weeks"));
-    $rangeQuery = "AND timestamp_list.datetime BETWEEN '$nextSunday' AND '$twoSaturdayFromThisSaturday'";
-} elseif ($range === 'specificDate') {
-    $date0 = date($dateFormat, $_POST['date0']);
-    $date1 = date($dateFormat, $_POST['date1']);
-    $rangeQuery = "AND timestamp_list.datetime BETWEEN '$date0' AND '$date1'";
-} elseif ($range === 'w2d') {
-    $date0 = date($dateFormat, strtotime('monday 00:00:00'));
-    $date1 = date($dateFormat, strtotime('today 23:59:59'));
-    $rangeQuery = "AND timestamp_list.datetime BETWEEN '$date0' AND '$date1'";
-}
-
-$query = "SELECT timestamp_list.stamp_id,timestamp_list.datetime,timestamp_list.stamp_special,timestamp_list.stamp_department FROM timestamp_list WHERE timestamp_list.user_id_stamp = $employeeId " . $rangeQuery . ' ORDER BY timestamp_list.datetime';
-$queryResult = mysqli_query($sqlConnection, $query);
-if (mysqli_num_rows($queryResult) !== false) {
+$dateFormat = 'Y-m-d';
+$timeFormat24 = 'H:i:s';
+$timeFormat12 = 'h:i:s a';
+$dateTimeFormat24 = $dateFormat.' '.$timeFormat24;
+if (sessionCheck()) {
+    $employeeId = $_COOKIE['xvtss'];
+    $range = $_POST['range'];
+    // todo: this might be a tad screwy because of timezones
+    $day = date('w');
+    $date0 = date($dateTimeFormat24, strtotime("-$day days"));
+    $date1 = date($dateTimeFormat24, strtotime('+'.(6-$day).' days'));
+    //$date0 = date($dateTimeFormat24, strtotime('last sunday'));
+    //$date1 = date($dateTimeFormat24, strtotime('next saturday 23:59:59'));
+    if ($range === 'last') {
+        $date0 = date($dateTimeFormat24, strtotime("-$day days -1 weeks")); //'last sunday -1 weeks'));
+        $date1 = date($dateTimeFormat24, strtotime('-'.($day+1).' days 23:59:59')); //'last sunday -1 days 23:59:59'));
+    } elseif ($range === 'next') {
+        $date0 = date($dateTimeFormat24, strtotime("-$day days +1 weeks")); //'next sunday'));
+        $date1 = date($dateTimeFormat24, strtotime('+'.(6-$day).' days +1 weeks 23:59:59')); //"next saturday 23:59:59 +1 weeks"));
+    } elseif ($range === 'specificDate') {
+        $date0 = date($dateTimeFormat24, $_POST['date0']);
+        $date1 = date($dateTimeFormat24, $_POST['date1']);
+    } elseif ($range === 'w2d') {
+        // todo: is this REALLY needed?
+        $date0 = date($dateTimeFormat24, strtotime('monday 00:00:00'));
+        $date1 = date($dateTimeFormat24, strtotime('today 23:59:59'));
+    }
+    $query = "SELECT stamp_id,timestamp_list.datetime,stamp_special,stamp_department,stamp_partner
+              FROM timestamp_list
+              WHERE user_id_stamp = $employeeId
+              AND timestamp_list.datetime BETWEEN '$date0' AND '$date1'
+              ORDER BY timestamp_list.datetime";
+    $queryResult = mysqli_query($sqlConnection, $query);
+    if ($queryResult === false) {
+        // If the query was somehow broken, just die.
+        return;
+    }
     $timestamps = [];
-    while (list($stampId, $timestamp, $modifier, $depart) = mysqli_fetch_row($queryResult)) {
+    $stamps = [];
+    while (list($stampId, $timestamp, $modifier, $department, $partnerId) = mysqli_fetch_row($queryResult)) {
         $lastTimestamp = end($timestamps);
-        $thisDay = date('Y-m-d', strtotime($timestamp) - 18000);
-        $thisTime = date('h:i:s a', strtotime($timestamp) - 18000);
-        if ($lastTimestamp['date'] === $thisDay && count($lastTimestamp) - 1 < 6) {
-            $timestamps[count($timestamps) - 1]['date'] = $thisDay;
-            array_push($timestamps[count($timestamps) - 1], [$stampId, $thisTime, $modifier, $depart]);
+        // We need the offset from the current timezone to GMT
+        $dateEDT = new DateTimeZone('America/New_York'); // todo: make timezone configurable
+        $offsetSeconds = $dateEDT->getOffset(new DateTime('now'));
+        $thisDay = date($dateFormat, strtotime($timestamp) + $offsetSeconds);
+        $thisTime = date($timeFormat12, strtotime($timestamp) + $offsetSeconds);
+        $thisUnix = date('U', strtotime($timestamp) + $offsetSeconds);
+        if ($lastTimestamp['date'] === $thisDay || in_array($partnerId, $stamps)) {
+            // push things in for this day.  They can be for this day, or reference it
+            $timestampsCount = count($timestamps) - 1;
+            array_push($timestamps[$timestampsCount], [$stampId, $thisUnix, $modifier, $department, $partnerId]);
         } else {
-            array_push($timestamps, ['date' => $thisDay, [$stampId, $thisTime, $modifier, $depart]]);
+            // if this is a new day (or it doesn't reference the previous day) then start a new day
+            array_push($timestamps, ['date'=>$thisDay, [$stampId, $thisUnix, $modifier, $department, $partnerId]]);
+        }
+        //  We need the stampIds to tell if the stamps reference one another
+        array_push($stamps, $stampId);
+    }
+    // todo: breaks could be an issue here!
+    // fixme: If there are no stamps, array_splice errors our!
+    /*
+    if (!in_array($timestamps[0][0][4], $stamps)) {
+        // Remove the first entry of the array if it isn't right!
+        array_splice($timestamps[0], 0, 1);
+        if (empty($timestamps[0])) {
+            // todo: this doesn't work properly because of [date]
+            // If we left the first array empty, get rid of it
+            array_splice($timestamps, 0, 1);
         }
     }
-
-    $quickQuery = "SELECT user_first,user_last,user_start FROM employee_list WHERE user_id = $employeeId";
-    $quickResult = mysqli_query($sqlConnection, $quickQuery);
-    if (mysqli_num_rows($quickResult) !== 0) {
-        list($userfirst, $userlast, $userstart) = mysqli_fetch_row($quickResult);
+    */
+    // todo: look into sunday of next week for matching stamps also
+    $employeeNameQuery = "SELECT user_first, user_last, user_start
+                          FROM employee_list
+                          WHERE user_id = $employeeId";
+    $nameResult = mysqli_query($sqlConnection, $employeeNameQuery);
+    if (mysqli_num_rows($nameResult) !== 0) {
+        list($userFirst, $userLast, $userStart) = mysqli_fetch_row($nameResult);
     } else {
-        $userfirst = "null";
-        $userlast = "null";
-        $userstart = "1970-01-01";
+        $userFirst = 'null';
+        $userLast = 'null';
+        $userStart = '1970-01-01';
     }
-
-    $addAfter = date($dateFormat, strtotime($timestamps[count($timestamps) - 1]['date'] . ' 00:00:00 +1 days'));
-    $echoMe = "<table id=\"timecard\" user-start=\"$userstart\" user-id=\"$employeeId\"><tr id=\"topTR\"><th id=\"topTH\" colspan=\"100%\">$userfirst $userlast's Timecard</th></tr><tr id=\"headings\"><th></th><th>Date<input id=\"r\" size=\"1\" style=\"width:0;height:0;display:none\"/><input id=\"r2\" size=\"1\" style=\"width:0;height:0;display:none\"/><select id=\"range\"><option value=\"last\">Previous Period</option><option value=\"this\" selected>Current Period</option><option value=\"next\">Next Period</option><option class=\"sp\" value=\"specificDate\">Specific Date</option><option value=\"w2d\">Week to Date</option><option value=\"special\">Specific Period</option></select></th><th colspan=\"100%\"></th></tr>";
-    $rowNumber = 0;
+    // Make sure this is here before anything
+    $echoMe = '';
+    // Add the datepicker stuff.
+    $echoMe .= '<input type="date" id="date0" class="datepicker"><input type="date" id="date1" class="datepicker">';
+    // Add the table's opening tag
+    $echoMe .= "<table id=\"timecard\" user-id=\"$employeeId\">"; // todo: is there a better way to store the id?
+    // Add the header, sans range selector
+    $echoMe .= "<tr id=\"topTR\"><th id=\"topTH\" colspan=\"100%\">$userFirst $userLast's Timecard";
+    // Add the select, close the header
+    $echoMe .= '<select id="range" class="browser-default">
+                 <option value="last">Previous Period</option>
+                 <option value="this">Current Period</option>
+                </select></th></tr>';
+    /*
+     *           <option value="next">Next Period</option>
+                 <option value="w2d">Week to Date</option>
+                 <option value="specificDate">Specific Date</option>
+                 <option value="special">Specific Period</option>
+     */
+    // Finally for the head, the (shitty) header for date and junk
+    $echoMe .= '<tr><th>Date</th><th colspan="100%"></th></tr>';
+    /*
+     * Total addition for hours
+     */
     $runningTotal = 0;
+    $timestampsCount = count($timestamps);
+    // We need the maximum number of stamps in a 'day', use this below to fill in rows that don't have as many
+    $maxStamps = 0;
+    foreach ($timestamps as $t) {
+        $countT = count($t);
+        if ($countT > $maxStamps) {
+            $maxStamps = $countT;
+        }
+    }
     foreach ($timestamps as $timestamp) {
-        $rowNumber++;
         $day = $timestamp['date'];
-        $addBefore = date($dateFormat, strtotime($day . ' 00:00:00 -1 days'));
-        $isOdd = ($rowNumber % 2 ? "" : 'odd');
-        $modDay = date('D m/d', strtotime($day));
-        $echoMe .= "<tr stamp-day=\"$day\" class=\"dataRow $isOdd\"><td class=\"dayCell\" id=\"$day\">$modDay</td>";
-        $timeIns = [];
-        $timeOuts = [];
-        $matchingSpecial = true;
-        $chosenModifier = '';
-        foreach ($timestamp as $t) {
-            if ($t[2] != $chosenModifier) {
-                $matchingSpecial = false;
-                $chosenModifier = $t[2];
-            } else {
-                $matchingSpecial = true;
+        $dayFormatted = date('D m/d', strtotime($day));
+        // Add the date column to $echoMe
+        // todo: redo parts of code to use the day cell's id to remove 'stamp-day'
+        $echoMe .= "<tr stamp-day=\"$day\" class=\"dataRow\">";
+        //$echoMe .= '<td class="newDate"><input class="newDate" type="button" value="<+"</td>';
+        $echoMe .= "<td class=\"dayCell\" id=\"$day\">$dayFormatted</td>";
+        $timestampCount = count($timestamp);
+        $timeIn = [];
+        $timeOut = [];
+        foreach ($timestamp as $key => $stamp) {
+            $miss = '';
+            // todo: figure out how this works! Past me was obviously smarter
+            if (($timestampCount % 2 === 0 && $key === $timestampCount - 2)) {
+                $miss = 'missingTime';
             }
-        }
-
-        $tyme = 0;
-        $c_timestamp = count($timestamp);
-        for ($i = 0; $i < $c_timestamp - 1; ++$i) {
-            $t_sid = $timestamp[$i][0];
-            $t_tms = $timestamp[$i][1];
-            $t_mod = $timestamp[$i][2];
-            $t_dep = $timestamp[$i][3];
-            $miss = "";
-            if (($c_timestamp % 2 === 0 && $i === $c_timestamp - 2) || !$matchingSpecial) {
-                $miss = "missingTime";
-            }
-
-            $echoMe .= "<td department-special=\"$t_dep\" class=\"tstable $t_mod $miss times\">";
-            if ($t_mod === 'S') {
-                $val = 'SICK';
-                $disabled = 'readonly disabled';
-            } elseif ($t_mod === 'F') {
-                $val = 'BEREAVEMENT';
-                $disabled = 'readonly disabled';
-            } elseif ($t_mod === 'V') {
-                $val = 'VACATION';
-                $disabled = 'readonly disabled';
-            } else {
-                $val = $t_tms;
+            $modifier = $stamp[2];
+            // ['date'] will count as a $stamp, so make sure we are dealing with an array
+            if (is_array($stamp)) {
+                //$echoMe .= '<td class="tstable addButton"><input class="addButton" type="button" value="+"></td>';
+                $realTime = date($timeFormat12, $stamp[1]);
+                $tri = '';
+                if (date($dateFormat, $stamp[1]) !== $day) {
+                    $tri = 'overnight';
+                }
+                $val = $realTime;
                 $disabled = '';
+                if ($modifier !== '') {
+                    $disabled = 'readonly disabled';
+                    if ($modifier === 'S') {
+                        $val = 'SICK';
+                    } elseif ($modifier === 'F') {
+                        $val = 'BEREAVEMENT';
+                    } elseif ($modifier === 'V') {
+                        $val = 'VACATION';
+                    }
+                }
+                $echoMe .= "<td class=\"droppableTimes times tstable $tri $miss\">
+                             <input readonly disabled class=\"times context-menu\" stamp-id=\"$stamp[0]\" id=\"$stamp[0]\" type=\"text\" value=\"$val\"></div>
+                            </td>";
+                if ($key % 2) {
+                    array_push($timeOut, $stamp[1]);
+                } else {
+                    array_push($timeIn, $stamp[1]);
+                }
             }
-
-            $echoMe .= "<input readonly disabled title=\"$t_mod\" alt=\"$t_mod\" class=\"times\" default-time=\"$t_tms\" stamp-id=\"$t_sid\" type=\"text\"  maxlength=\"11\" id=\"$t_sid\" value=\"" . $val . "\"></td>";
-            if ($i % 2) {
-                array_push($timeOuts, strtotime($t_tms));
-            } else {
-                array_push($timeIns, strtotime($t_tms));
-            }
-
-            $tyme = $i;
         }
-
+        //$echoMe .= '<td class="addButton after"><input class="addButton" type="button" value="+"></td>';
         $timeTotal = 0;
-        for ($i = 0; $i < count($timeOuts); ++$i) {
-            $timeTotal += ($timeOuts[$i] - $timeIns[$i]);
+        for ($i = 0; $i < count($timeOut); ++$i) {
+            $timeTotal += ($timeOut[$i] - $timeIn[$i]);
         }
-
-        for ($i = 0; $i < 5 - $tyme; ++$i) {
+        for ($i = 0; $i < $maxStamps - $timestampCount; ++$i) {
+            // Fill in rows that don't have the maximum number of stamps.
             $echoMe .= '<td colspan=2 class="overflow"></td>';
         }
-
-        $timeTotal *= (($chosenModifier === 'H' && (strtotime($day . ' 00:00:00') - strtotime($userstart . ' 00:00:00')) >= 7776000) ? 2 : 1);
         $timeTotal = round($timeTotal / 3600, 2);
-        $echoMe .= '<td class="dailyHours" colspan"99%">';
+        $echoMe .= '<td class="dailyHours">';
         $echoMe .= number_format($timeTotal, 2);
-        $echoMe .= ' hours</td></tr>';
+        $echoMe .= '</td>';
+        $echoMe .= '</tr>';
         $runningTotal += $timeTotal;
     }
-
-    $echoMe .= '<tr class="dataRow"><td colspan="15"></td><td class="dailyHours">';
+    //$echoMe .= '<tr class="dataRow"><td class="newDate after" colspan="100%"><input class="newDate after" type="button" value="+"</td></tr>';
+    $push = $maxStamps;
+    $echoMe .= "<tr class=\"dataRow\"><td colspan=\"$push\"></td><td class=\"dailyHours\">";
     $echoMe .= number_format($runningTotal, 2);
-    $echoMe .= ' hours</td></tr><tr><th colspan="100%">Timecard</th></tr></table><p style="margin:0;font-size:50%;">written by: Brian Maurer</p>';
+    $echoMe .= '</td></tr><tr><th colspan="100%">Timecard</th></tr>';
+    $echoMe .= '</table>';
+    mysqli_close($sqlConnection);
+    echo $echoMe;
 }
-echo $echoMe;
