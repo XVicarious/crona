e5bc85948b40cc3968227452af094f722c1bdd04
Brian Maurer <bmaurer@xvss.net>
2015-09-24 15:35:08 -0400

    the admin id is in the session!

--- admin/lock_timecard.php
+++ admin/lock_timecard.php
@@ -3,11 +3,12 @@ require "admin_functions.php";
 $sqlConnection = createSql();
 if (sessionCheck()) {
     $apt_user = $_POST['user']; // the user's card being locked
-    $apt_admin = $_POST['admin']; // the admin doing the locking
+    $apt_admin = $_SESSION['userId']; // the admin doing the locking
     $apt_year = $_POST['year']; // the year the timecard falls in (YYYY)
     $apt_week = $_POST['week']; // the week of the year the timecard falls in 01 - 52
     $apt_locked = $_POST['locked'] || false; //  is this the FINAL lock?  If not, this is false
     // is this the best way to do this?  probably not, but I don't want to double dip in the query
+    // todo: this is something that should be checked for an admin
     if ($apt_locked) {
         $lock_query = "UPDATE approved_timecards SET apt_locked = TRUE
                        WHERE apt_user = $apt_user AND apt_year = $apt_year AND apt_week = $apt_week";
