10fadc1947d1c9c25c4edf52566f86daacd6f91c
Brian Maurer <bmaurer@xvss.net>
2015-10-01 16:40:49 -0400

    attempting to overhaul insert_user to the new database structure.  Not doing too well.

--- admin/insert_user.php
+++ admin/insert_user.php
@@ -18,9 +18,28 @@ if (sessionCheck()) {
         $userADPID = $a_postUser[4];
         $userPassword = randomSalt(false);
         $salt = randomSalt();
-        $userPassword = $salt . sha1($salt . $userPassword);
+        $userPassword = sha1($salt . $userPassword);
         $username = generateUsername($sqlConnection, substr($userFirst, 0, 1) . $userLast);
-        $query = 'INSERT INTO employee_list (user_name,user_adpid,user_companycode,user_department,user_password,
+
+        $checkEmailQuery = "SELECT ueml_id FROM user_emails WHERE ueml_email='$userEmail'";
+        $result = mysqli_query($sqlConnection, $checkEmailQuery);
+        if (mysqli_num_rows($result) > 0) {
+            $userEmail = mysqli_fetch_assoc($result); // what does this return again?
+            $userEmail = $userEmail['ueml_id'];
+        } else {
+            $emailQuery = "INSERT INTO user_emails (ueml_email) VALUES ('$userEmail')
+                           SELECT ueml_id FROM user_emails WHERE ueml_email = '$userEmail'";
+            $result = mysqli_query($sqlConnection, $emailQuery);
+            $userEmail = mysqli_fetch_assoc($result); // errr?
+            $userEmail = $userEmail['ueml_id'];
+        }
+        $time = time();
+        $query = "INSERT INTO employee_list (user_name,user_adpid,user_companycode,user_department,user_first,user_last)
+                  VALUES ($username,$userADPID,$userCompany,$userDepartment,$userFirst,$userLast);
+                  INSERT INTO user_hashes (uhsh_user,uhsh_hash,uhsh_created) VALUES (
+                  (SELECT user_id FROM employee_list WHERE user_name = '$username'), $userPassword, $time);";
+        echo $query;
+        /*$query = 'INSERT INTO employee_list (user_name,user_adpid,user_companycode,user_department,user_password,
                   user_first,user_last';
         $queryPartTwo = "VALUES ('$username',$userADPID,'$userCompany',$userDepartment,'$userPassword','$userFirst',
                          '$userLast'";
@@ -35,7 +54,7 @@ if (sessionCheck()) {
         $query .= ') ';
         $queryPartTwo .= ')';
         $query .= $queryPartTwo;
-        mysqli_query($sqlConnection, $query);
+        mysqli_query($sqlConnection, $query);*/
     }
     mysqli_close($sqlConnection);
 }
