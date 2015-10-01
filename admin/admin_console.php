9d5f2656c2335681e18a51288b4251b5c308aeb7
Brian Maurer <bmaurer@xvss.net>
2015-10-01 16:34:56 -0400

    load the configuration for the admin console

--- admin/admin_console.php
+++ admin/admin_console.php
@@ -1,17 +1,23 @@
-<script src="../js/lib/jquery.js"></script>
-<script src="../js/lib/materialize.js"></script>
 <?php
-$echoMe = '<ul class="collapsible" data-collapsible="accordion">
+
+$jsonString = file_get_contents('settings.json');
+$settings = json_decode($jsonString, true);
+
+?>
+<ul class="collapsible popout" data-collapsible="accordion">
     <li>
-        <div class="collapsible-header"><i class="mdi-action-schedule"></i>Timestamp Thresholds</div>
+        <div class="collapsible-header active"><i class="mdi-action-schedule"></i>Timestamp Thresholds</div>
         <div class="collapsible-body">
+            <p>
             Stamp In Thresholds:<br/>
-            <label for="in-minus-threshold">Before: </label><input id="in-minus-threshold" name="in-before" value="0" size="1"><br/>
-            <label for="in-plus-threshold">After: </label><input id="in-plus-threshold" name="in-after" value="0" size="1"><br/>
+            <label for="in-minus-threshold">Before: </label><input id="in-minus-threshold" name="in-before" value="<? echo $settings["stampThresholds"]["timeIn"]["early"] ?>" size="1"><br/>
+            <label for="in-plus-threshold">After: </label><input id="in-plus-threshold" name="in-after" value="<? echo $settings["stampThresholds"]["timeIn"]["late"] ?>" size="1"><br/>
             <br/>
             Stamp Out Thresholds:<br/>
-            <label for="out-minus-threshold">Before: </label><input id="out-minus-threshold" name="out-before" value="0" size="1"><br/>
-            <label for="out-plus-threshold">After: </label><input id="out-plus-threshold" name="out-after" value="0" size="1"><br/>
+            <label for="out-minus-threshold">Before: </label><input id="out-minus-threshold" name="out-before" value="<? echo $settings["stampThresholds"]["timeOut"]["early"] ?>" size="1"><br/>
+            <label for="out-plus-threshold">After: </label><input id="out-plus-threshold" name="out-after" value="<? echo $settings["stampThresholds"]["timeOut"]["late"] ?>" size="1"><br/><br/>
+            <a href="#" class="btn"><i class="material-icons prefix left">save</i>Save</a>
+            </p>
         </div>
     </li>
     <li>
@@ -33,7 +39,4 @@ $echoMe = '<ul class="collapsible" data-collapsible="accordion">
             <a class="cyan lighten-1 waves-effect waves-light btn" id="timestamp-button">Open Timestamp Log</a>
         </div>
     </li>
-</ul>';
-echo $echoMe;
-
-// Does this need to be a PHP file? likely not
+</ul>
