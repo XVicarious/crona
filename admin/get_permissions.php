dd7c45ea95b3b38069a549ca17f4ed4607c56590
Brian Maurer <bmaurer@xvss.net>
2015-10-06 13:46:44 -0400

    converted to PDO

--- admin/get_permissions.php
+++ admin/get_permissions.php
@@ -1,13 +1,23 @@
 <?php
 require "admin_functions.php";
-$sqlConnection = createSql();
+include 'SqlStatements.php';
 if (sessionCheck()) {
     $userId = $_SESSION['userId'];
-    $query = "SELECT company_code, department_id FROM employee_supervisors WHERE user_id = $userId";
-    $result = mysqli_query($sqlConnection, $query);
+    $dbh = createPDO();
+    try {
+        $stmt = $dbh->prepare(SqlStatements::GET_CHECK_PERMISSIONS, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $stmt->bindParam(':userid', $userId, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
+    } catch (Exception $e) {
+        javascriptLog('Failed: '.$e->getMessage());
+    }
     $permissionArray = [];
-    while(list($companyCode, $departmentId) = mysqli_fetch_row($result)) {
+    /*while(list($companyCode, $departmentId) = mysqli_fetch_row($result)) {
         array_push($permissionArray, [$companyCode, $departmentId]);
     }
     echo json_encode($permissionArray);
+    */
+    $dbh = null;
 }
