02afe26b967d210cff15e67f8b3eabfa8483ee43
Brian Maurer <bmaurer@xvss.net>
2015-10-16 16:09:44 -0400

    moved change_stamp to PDO

--- admin/timeEdit/change_stamp.php
+++ admin/timeEdit/change_stamp.php
@@ -1,13 +1,20 @@
 <?php
 date_default_timezone_set('Atlantic/Reykjavik');
 require "../admin_functions.php";
-$sqlConnection = createSql();
+include '../SqlStatements.php';
 if (sessionCheck()) {
     $stampId = $_POST['sid'];
     $defaultTime = $_POST['dtime'];
     $time = date("Y-m-d H:i:s", $_POST['time']);
-    mysqli_query($sqlConnection, "UPDATE timestamp_list SET tsl_stamp = '$time'
-                                  WHERE stamp_id = $stampId");
-    logTransaction($sqlConnection, $stampId, "CHANGE", $defaultTime, $time);
-    mysqli_close($sqlConnection);
+    $dbh = createPDO();
+    try {
+        $stmt = $dbh->prepare(SqlStatements::MODIFY_STAMP, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $stmt->bindParam(':stamp', $time, PDO::PARAM_STR);
+        $stmt->bindParam(':stampid', $stampId, PDO::PARAM_INT);
+        $stmt->execute();
+        logTransaction($sqlConnection, $stampId, "CHANGE", $defaultTime, $time);
+    } catch (PDOException $e) {
+        error_log($e->getMessage(), 0);
+    }
+    $dbh = null;
 }
