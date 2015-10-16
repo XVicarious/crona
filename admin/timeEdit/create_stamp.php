702c49ec91033f2418707951956411708da3d98d
Brian Maurer <bmaurer@xvss.net>
2015-10-16 16:04:09 -0400

    moved create stamp to PDO

--- admin/timeEdit/create_stamp.php
+++ admin/timeEdit/create_stamp.php
@@ -1,12 +1,19 @@
 <?php
 date_default_timezone_set('Atlantic/Reykjavik');
 require "../admin_functions.php";
-$sqlConnection = createSql();
+include '../SqlStatements.php';
 if (sessionCheck()) {
     $dtime = $_POST['date'];
     $date = date("Y-m-d H:i:s", $dtime);
     $userId = $_POST['user'];
-    mysqli_query($sqlConnection, "INSERT INTO timestamp_list (user_id_stamp,tsl_stamp) VALUES ($userId,'$date')");
-    logTransaction($sqlConnection, '?', 'ADD', 0, $date);
-    mysqli_close($sqlConnection);
+    $dbh = createPDO();
+    try {
+        $stmt = $dbh->prepare(SqlStatements::INSERT_NEW_STAMP, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $stmt->bindParam(':userid', $userId, PDO::PARAM_INT);
+        $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+        $stmt->execute();
+        logTransaction($sqlConnection, '?', 'ADD', 0, $date);
+    } catch (PDOException $e) {
+        error_log($e->getMessage(), 0);
+    }
 }
