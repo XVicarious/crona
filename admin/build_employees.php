3f568d03ee9d9cda6b0362bc3bf90e42517254e5
Brian Maurer <bmaurer@xvss.net>
2015-12-03 13:16:28 -0500

    get rid of the awful horrible backtable grid that caused problems 1/2

---
+++ admin/build_employees.php
@@ -0,0 +1,28 @@
+<?php
+include 'admin_functions.php';
+if (sessionCheck()) {
+    $json = $_SESSION['json'];
+    $table = '<table id="employees-all" class="bordered centered responsive-table highlight">
+                  <thead>
+                      <tr>
+                          <th data-field="checkbox"><p><input type="checkbox" id="checkall"/><label for="checkall"/></p></th>
+                          <th data-field="id">ID</th>
+                          <th data-field="adpid">ADP ID</th>
+                          <th data-field="name">Name</th>
+                          <th data-field="companycode">Company Code</th>
+                          <th data-field="department">Deparment Code</th>
+                      </tr>
+                  </thead>
+                  <tbody>';
+    $employees = json_decode($json, true);
+    foreach ($employees as $person) {
+        $t_id = $person['id'];
+        $t_adp = $person['adpid'];
+        $t_name = $person['name'];
+        $t_company = $person['companycode'];
+        $t_department = $person['departmentcode'];
+        $_checkbox = "<p><input type=\"checkbox\" id=\"id$t_id\"/><label for=\"id$t_id\"></label></p>";
+        $table .= "<tr user-id=\"$t_id\"><td>$_checkbox</td><td>$t_id</td><td>$t_adp</td><td>$t_name</td><td>$t_company</td><td>$t_department</td></tr>";
+    }
+    echo $table.'</tbody></table>';
+}
--- admin/get_employees.json.php
+++ admin/get_employees.json.php
@@ -1,9 +1,11 @@
 <?php
 // todo: update SQL
-require "admin_functions.php";
-$sqlConnection = createSql();
+require 'admin_functions.php';
+include 'SqlStatements.php';
 if (sessionCheck()) {
+    $sqlConnection = createSql();
     $administrativeId = $_SESSION['userId'];
+    //$dbh = createPDO();
     $permissionQuery = "SELECT company_code, department_id FROM employee_supervisors WHERE user_id = $administrativeId";
     $result = mysqli_query($sqlConnection, $permissionQuery);
     $permissionsArray = [];
@@ -44,21 +46,18 @@ if (sessionCheck()) {
     if ($resultNumber !== 0) {
         // Start generating that table monkey!
         $totalPages = ceil($resultNumber / 10);
-        array_push($a_employeeList, ["per_page" => 10,
-                                     "total_entries" => $resultNumber,
-                                     "total_pages" => $totalPages,
-                                     "page" => 1]);
-        array_push($a_employeeList, []);
         while (list($user_id,$user_adpid,$user_last,$user_first,$user_companycode,$user_department) = mysqli_fetch_row($result)) {
-            array_push($a_employeeList[1], ['id' => $user_id,
+            array_push($a_employeeList, ['id' => $user_id,
                                             'adpid' => $user_adpid,
                                             'name' => "$user_last, $user_first",
-                                            'companycode' => $user_companycode,
+                                            'companycode' => "$user_companycode",
                                             'departmentcode' => $user_department]);
         }
     } else {
         echo 'No employees found!';
     }
-    echo json_encode($a_employeeList, JSON_NUMERIC_CHECK);
+    //echo json_encode($a_employeeList, JSON_NUMERIC_CHECK);
+    $_SESSION['json'] = json_encode($a_employeeList, JSON_NUMERIC_CHECK);
     mysqli_close($sqlConnection);
+    header('Location: build_employees.php');
 }
