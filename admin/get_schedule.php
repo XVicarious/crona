424c49506d50e3d33773a64df40623aa5798d28a
Brian Maurer <bmaurer@xvss.net>
2015-12-22 12:18:08 -0500

    super optimized this.  Holy crap that was inefficient.

--- admin/get_schedule.php
+++ admin/get_schedule.php
@@ -17,60 +17,15 @@ if (sessionCheck()) {
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $fixedSchedule = [];
-        foreach ($result as $scheduleDay) {
-            schErr($employee, 'Starting loop for $result');
-            schErr($employee, json_encode($scheduleDay, true));
-            $last = end($fixedSchedule);
-            schErr($employee, '$last is '.$last);
-            if (!empty($last)) {
-                schErr($employee, '$last is not null (end($fixedSchedule))');
-                if ($scheduleDay['schedule_year'] === $last['schedule_year'] &&
-                    $scheduleDay['schedule_week'] === $last['schedule_week']) {
-                    schErr($employee, 'week and year match $last and this');
-                    // to check if there is more than one day between the previous day and the day we are inputting
-                    $absoluteDayGap = abs($scheduleDay['schedule_day'] - $last['schedule_day']);
-                    schErr($employee, '$scheduleDay[\'schedule_day\']='.$scheduleDay['schedule_day']);
-                    schErr($employee, '$last[0][\'schedule_day\']='.$last['schedule_day']);
-                    if ($scheduleDay['schedule_day'] === $last['schedule_day']) {
-                        schErr($employee, 'the days match as well');
-                        $fixedScheduleCount = count($fixedSchedule) - 1;
-                        error_log('pushing to already created array', 0);
-                        array_push($fixedSchedule, $scheduleDay);
-                    } elseif ($absoluteDayGap > 1) {
-                        schErr($employee, 'The days don\'t match and the gap is greater than 1, it is '.$absoluteDayGap);
-                        $whileGap = $absoluteDayGap;
-                        while ($whileGap > 1) {
-                            schErr($employee, 'pushing a schedule day in with $whileGap='.$whileGap);
-                            array_push($fixedSchedule, ['schedule_day' => $last['schedule_day']+$whileGap]);
-                            $whileGap--;
-                        }
-                        schErr($employee, '$whileGap under control...  adding the stamp that needs adding now');
-                        array_push($fixedSchedule, $scheduleDay);
-                    } else {
-                        schErr($employee, 'the days don\'t match, but the gap is only 1 so we are fine');
-                        array_push($fixedSchedule, $scheduleDay);
-                    }
-                } else {
-                    array_push($fixedSchedule, $scheduleDay);
-                }
-            } else {
-                $whileGap = $scheduleDay['schedule_day'];
-                $currentWhileDay = 1;
-                while ($whileGap > $currentWhileDay) {
-                    array_push($fixedSchedule, ['schedule_day' => $currentWhileDay++]);
-                }
-                array_push($fixedSchedule, $scheduleDay);
-            }
-        }
-        $dayArray = [1,2,3,4,5,6,7];
+        $dayArray = ['1','2','3','4','5','6','0'];
         foreach ($dayArray as $weekDay) {
-            if (!array_search($weekDay, array_column($fixedSchedule, 'schedule_day'))) {
-                array_push($fixedSchedule, ['schedule_day' => $weekDay]);
+            if (array_search($weekDay, array_column($result, 'schedule_day')) === false) {
+                array_push($result, ['schedule_day' => $weekDay]);
             }
         }
-        usort($fixedSchedule, 'csort');
-        array_unshift($fixedSchedule, ['year'=>$year, 'week'=>$week, 'userid'=>$employee]);
-        echo json_encode($fixedSchedule, JSON_NUMERIC_CHECK);
+        usort($result, 'csort');
+        array_unshift($result, ['year'=>$year, 'week'=>$week, 'userid'=>$employee]);
+        echo json_encode($result, JSON_NUMERIC_CHECK);
     } catch (PDOException $e) {
         error_log($e->getMessage(), 0);
     } catch (Exception $e) {
@@ -79,7 +34,7 @@ if (sessionCheck()) {
     }
 }
 
-function schErr($user,$message)
+function schErr($user, $message)
 {
     error_log('get_schedule.php id='.$user.' : '.$message, 0);
 }
