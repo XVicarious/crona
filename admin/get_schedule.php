9d100fe8bc54aa433a2b887e79b8f2838e54f3fe
Brian Maurer <bmaurer@xvss.net>
2016-01-22 12:06:13 -0500

    updates for new new storage of schedules

--- admin/get_schedule.php
+++ admin/get_schedule.php
@@ -5,27 +5,46 @@ if (sessionCheck()) {
     // Use this to select the starting page
     // todo: fringe cases where the year changes in the middle of the week.  These dates will not be fetched!
     $employee = $_POST['userId'];
-    $year = $_POST['ustart'];
-    $week = $_POST['uend'];
+    $unixStart = $_POST['ustart'];
+    $unixEnd = $_POST['uend'];
     $dbh = createPDO();
     $result = null;
     try {
-        $stmt = $dbh->prepare(SqlStatements::GET_SCHEDULE, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $stmt = $dbh->prepare(SqlStatements::GET_SCHEDULE_2, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
         $stmt->bindParam(':userid', $employee, PDO::PARAM_INT);
-        $stmt->bindParam(':sweek', $week, PDO::PARAM_INT);
-        $stmt->bindParam(':syear', $year, PDO::PARAM_INT);
+        $stmt->bindParam(':ustart', $unixStart, PDO::PARAM_INT);
+        $stmt->bindParam(':uends', $unixEnd, PDO::PARAM_INT);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $fixedSchedule = [];
-        $dayArray = ['1','2','3','4','5','6','0'];
-        foreach ($dayArray as $weekDay) {
-            if (array_search($weekDay, array_column($result, 'schedule_day')) === false) {
-                array_push($result, ['schedule_day' => $weekDay]);
+        foreach ($result as $item) {
+            if ($item['schedule_pair'] === null) {
+                error_log('null', 0);
+                array_push($fixedSchedule, [$item]);
+            } else {
+                $pairIndex = searchInArray($item['schedule_pair'], $fixedSchedule, 'schedule_id');
+                $indexPair = searchInArray($item['schedule_id'], $fixedSchedule, 'schedule_pair');
+                if (!($pairIndex === false)) {
+                    error_log('$pairIndex '.$pairIndex.' '.json_encode($item, true), 0);
+                    array_push($fixedSchedule[$pairIndex], $item);
+                } elseif (!($indexPair === false)) {
+                    error_log('$indexPair '.$indexPair.' '.json_encode($item, true), 0);
+                    array_push($fixedSchedule[$indexPair], $item);
+                } else {
+                    error_log('new '.json_encode($item, true), 0);
+                    array_push($fixedSchedule, [$item]);
+                }
             }
         }
-        usort($result, 'csort');
-        array_unshift($result, ['year'=>$year, 'week'=>$week, 'userid'=>$employee]);
-        echo json_encode($result, JSON_NUMERIC_CHECK);
+        //$dayArray = ['1','2','3','4','5','6','0'];
+        //foreach ($dayArray as $weekDay) {
+        //    if (array_search($weekDay, array_column($result, 'schedule_day')) === false) {
+        //        array_push($result, ['schedule_day' => $weekDay]);
+        //    }
+        //}
+        //usort($result, 'csort');
+        array_unshift($fixedSchedule, ['userid'=>$employee]);
+        echo json_encode($fixedSchedule, JSON_NUMERIC_CHECK);
     } catch (PDOException $e) {
         error_log($e->getMessage(), 0);
     } catch (Exception $e) {
@@ -42,4 +61,17 @@ function schErr($user, $message)
 function csort($left, $right, $column = 'schedule_day')
 {
     return $left[$column] > $right[$column];
+}
+
+function searchInArray($needle, $haystack, $key, $strict = false)
+{
+    error_log($needle.' '.json_encode($haystack, true).' '.$key);
+    foreach ($haystack as $index => $item) {
+        if (($strict ? $item[0][$key] === $needle : $item[0][$key] == $needle)) {
+            error_log($index, 0);
+            return $index;
+        }
+    }
+    error_log('not found', 0);
+    return false;
 }
