935ca1859bd5e714c0d660983e438f9ceefee761
Brian Maurer <bmaurer@xvss.net>
2016-01-29 14:29:10 -0500

    fixed the path after it was moved

--- forgot/insert_security.php
+++ forgot/insert_security.php
@@ -7,7 +7,7 @@ $answer2 = sha1(strtolower($_POST['s2i']));
 $question3 = $_POST['s3'];
 $answer3 = sha1(strtolower($_POST['s3i']));
 $userId = $_SESSION['userId'];
-require 'admin/admin_functions.php';
+require '../admin/admin_functions.php';
 $sqlConnection = createSql();
 $query = "INSERT INTO employee_questions (eque_number, eque_answer, eque_user) VALUES ($question1, '$answer1', $userId),
           ($question2, '$answer2', $userId), ($question3, '$answer3', $userId)";
