c64e8003cc4ee7805b29b33b9f354b1195abccec
Brian Maurer <bmaurer@xvss.net>
2016-02-05 14:22:41 -0500

    last of resetutil modified.  still one thing to do...

--- forgot/resetutil.php
+++ forgot/resetutil.php
@@ -83,38 +83,53 @@ if (isset($function)) {
         }
     }
 } elseif (isset($_GET['c'])) {
-    $resetId = $_GET['c'];
-    $query = "SELECT reset_uid,reset_string,reset_date FROM reset_list WHERE reset_string = '$resetId'";
-    $result = mysqli_query($sqlConnection, $query);
-    if (mysqli_num_rows($result) !== 0) {
-        list($uid, $resetId, $sdate) = mysqli_fetch_row($result);
-        $difference = time() - strtotime($sdate);
-        if ($difference >= 86400) {
-            $query = "DELETE FROM reset_list WHERE reset_string = '$resetId'";
-            mysqli_query($sqlConnection, $query);
-            echo 'The password reset link you gave has expired.  Please to go
-                  <a href="http://xvicario.us/time/forgot/?r=go">http://xvicario.us/time/forgot/
-                  ?r=go</a> to get a new link.';
-            return;
-        }
-        $random = rand(0, 2);
-        $securityId = "sec_$random";
-        $query = "SELECT sque_id, sque_question FROM security_questions WHERE sque_id IN
-                  (SELECT eque_number FROM employee_questions WHERE eque_user = $uid) LIMIT 1 OFFSET $random";
-        $result = mysqli_query($sqlConnection, $query);
-        echo $query;
-        list($securityId, $securityQuestion) = mysqli_fetch_row($result);
-        echo '<div id="rpassword" style="position:absolute;display:block"><form style="background-color:white">
+    $resetString = $_GET['c'];
+    $dbh = createPDO();
+    try {
+        $success = false;
+        $stmt = $dbh->prepare(SqlStatements::GET_RESET_INFORMATION);
+        $stmt->bindParam(':resetString', $resetString, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (count($result) > 0) {
+            $result = $result[0];
+            $difference = time() - strtotime($result['reset_date']);
+            if ($difference >= 86400) {
+                $stmt = $dbh->prepare(SqlStatements::DELETE_RESET_BY_STRING);
+                $stmt->bindParam(':resetString', $resetString, PDO::PARAM_STR);
+                $stmt->execute();
+                echo 'The password reset link you gave has expired, please request a new password reset.';
+                return;
+            } else {
+                $stmt = $dbh->prepare(SqlStatements::SELECT_RANDOM_SECURITY_QUESTION);
+                $stmt->bindParam(':userID', $uid, PDO::PARAM_INT);
+                $stmt->execute();
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+                $securityQuestion = $result['sque_question'];
+                $securityId = 'sec_0'; // need to figure all this junk out...
+                echo '<div id="rpassword" style="position:absolute;display:block"><form style="background-color:white">
               <input id="uid" type=hidden name="user" value="' . $uid . '"><input type=hidden name="function"
               value="checkReset"><table id="loginForm" style="border:solid thin black;table-layout:fixed;
               font-family:monospace"><tr></tr><tr><td>' . $securityQuestion . "</td><td>
               <input id=\"answer\" name=\"answer\" type=password></td></tr><tr><td>New Password:<input type=hidden
-              id=\"resetId\" name=\"resetId\" value=\"$resetId\"><input id=\"qid\" type=hidden name=\"qid\"
+              id=\"resetId\" name=\"resetId\" value=\"$resetString\"><input id=\"qid\" type=hidden name=\"qid\"
               value=\"$securityId\"></td><td><input id=\"pw\" type=password></td></tr><tr><td>Confirm New Password:</td><td>
               <input id=\"pwc\" type=password name=\"pword\"></td></tr><tr><th colspan=\"3\"><input id=\"subby\"
               style=\"width:100%\" type=submit value=\"Submit\"></th></tr></table></form></div>";
-    } else {
-        echo 'Invalid reset link.';
+            }
+        } else {
+            echo '*to the tune of the creepy Twilight Zone chime* do do do do, do do do do';
+        }
+        $success = true;
+    } catch (PDOException $e) {
+        error_log($e->getMessage(), 0);
+    } catch (Exception $e) {
+        error_log($e->getMessage(), 0);
+    } finally {
+        $dbh = null;
+        if (!$success) {
+            die();
+        }
     }
 }
 mysqli_close($sqlConnection);
