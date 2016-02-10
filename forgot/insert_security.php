6a170cc81d98cdfb41fc246b6ccab08a6f16b9f1
Brian Maurer <bmaurer@xvss.net>
2016-02-10 13:22:33 -0500

    security question setting and stuff SHOULD be good now...

--- forgot/insert_security.php
+++ forgot/insert_security.php
@@ -8,9 +8,30 @@ $question3 = $_POST['s3'];
 $answer3 = sha1(strtolower($_POST['s3i']));
 $userId = $_SESSION['userId'];
 require '../admin/admin_functions.php';
+require '../admin/SqlStatements.php';
 $sqlConnection = createSql();
 $query = "INSERT INTO employee_questions (eque_number, eque_answer, eque_user) VALUES ($question1, '$answer1', $userId),
           ($question2, '$answer2', $userId), ($question3, '$answer3', $userId)";
-echo "<script>console.log('$query')</script>";
 mysqli_query($sqlConnection, $query);
 mysqli_close($sqlConnection);
+$dbh = createPDO();
+try {
+    $stmt = $dbh->prepare(SqlStatements::INSERT_SECURITY_ANSWERS);
+    $stmt->bindParam(':userID', $userId, PDO::PARAM_INT);
+    $stmt->bindParam(':questionOne', $question1, PDO::PARAM_INT);
+    $stmt->bindParam(':questionTwo', $question2, PDO::PARAM_INT);
+    $stmt->bindParam(':questionThree', $question3, PDO::PARAM_INT);
+    $stmt->bindParam(':answerOne', $answer1, PDO::PARAM_STR);
+    $stmt->bindParam(':answerTwo', $answer2, PDO::PARAM_STR);
+    $stmt->bindParam(':answerThree', $answer3, PDO::PARAM_STR);
+    $stmt->execute();
+} catch (PDOException $e) {
+    error_log($e->getMessage(), 0);
+} catch (Exception $e) {
+    error_log($e->getMessage(), 0);
+} finally {
+    $dbh = null;
+    if (!$success) {
+        die();
+    }
+}
